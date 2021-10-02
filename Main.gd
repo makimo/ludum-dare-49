453d27377b3cb685cb0d04a2da4ae87a60886dc4
Bujno <0k.bujnowicz@gmail.com>
2021-10-02 21:01:05 +0200

    A little bit of refactoring

--- Main.gd
+++ Main.gd
@@ -11,19 +11,12 @@ func obstacle_or_not_obstacle() -> bool:
 	return randf() > 0.92
 
 func _on_Player_move() -> void:
+	obstacle_array = get_tree().get_nodes_in_group("OBS")
 	if obstacle_or_not_obstacle() and obstacle_array.size() < max_obstacle_objects:
-		print("CREATE NEW LIFE")
 		var obstacle = obstacle_scene.instance()
 		var obstacle_location = $SpawnPoints/LeftPoint if randi() % 2 == 0 else $SpawnPoints/RightPoint
 		
 		add_child(obstacle)
 		obstacle_array.append(obstacle)
 		obstacle.initialize(obstacle_location.translation)
-		
-	var obstacle_array_without_null = []
-	for obstacle in obstacle_array:
-		if is_instance_valid(obstacle):
-			obstacle.move()
-			obstacle_array_without_null.append(obstacle)
-	obstacle_array = obstacle_array_without_null
-	
+	get_tree().call_group("OBS", "move")
