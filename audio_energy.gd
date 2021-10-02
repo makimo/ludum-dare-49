146645ace751789071f984cfbca46cb349061cbb
Kiti <krzysztof@makimo.pl>
2021-10-02 23:30:25 +0200

    further work

--- audio_energy.gd
+++ audio_energy.gd
@@ -3,8 +3,8 @@ extends Node2D
 const VU_COUNT = 1024
 const FREQ_MAX = 11050.0
 
-const WIDTH = 400
-const HEIGHT = 100
+const WIDTH = 1024
+const HEIGHT = 200
 const LIMIT = 30 # one in every `limit` samples gets sent
 const MIN_DB = 55
 # probably some fourier parameters are fucked up, this coeff makes it good.
@@ -12,14 +12,14 @@ const MAGIC_KITI_NUMBER = 1.07
 
 var spectrum
 var curr_limit = 0
-
+var glob_mags = []
 
 
 signal audio_volume(value)
 signal audio_pitch(value)
 
 func calc_pitch_from_spec(mags, bin_size):
-	var THRESHOLD = 0.7
+	var THRESHOLD = 0.1
 	# leave only the peaks
 	var max_val = mags.max()
 	for i in range(0, len(mags)):
@@ -49,6 +49,16 @@ func calc_pitch_from_spec(mags, bin_size):
 	#	return (maxes[1] - maxes[0]) * bin_size
 	return 0
 	#return maxes[0] * bin_size
+	
+func _draw():
+	#warning-ignore:integer_division
+	if (len(glob_mags) == 0):
+		return
+	var w = WIDTH / VU_COUNT
+	for i in range(1, VU_COUNT+1):
+		var height = glob_mags[i-1] * HEIGHT
+		draw_rect(Rect2(w * i, HEIGHT - height, w, height), Color.white)
+		
 func _process(_delta):
 	var magnitude: float = spectrum.get_magnitude_for_frequency_range(0, 20000).length()
 	var final_energy = clamp((MIN_DB + linear2db(magnitude)) / MIN_DB, 0, 1)
@@ -66,12 +76,14 @@ func _process(_delta):
 		#if (energy > pitch_energy):
 		#	pitch = hz
 		#	pitch_energy = energy
+	glob_mags = mags
 	pitch = MAGIC_KITI_NUMBER * calc_pitch_from_spec(mags, FREQ_MAX/VU_COUNT)
 	curr_limit += 1
 	if (curr_limit >= LIMIT):
 		emit_signal("audio_volume", final_energy)
 		emit_signal("audio_pitch", pitch)
 		curr_limit = 0
+		#update()
 	
 func _ready():
 	spectrum = AudioServer.get_bus_effect_instance(1,0)
