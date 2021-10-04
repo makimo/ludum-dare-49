fac10ad4c6c2070e441dd8449dd668794163ae81
Katarzyna Bujnowicz <katarzyna.bujnowicz@makimo.pl>
2021-10-04 18:49:28 +0200

    Basic interface & die feature

--- Main.gd
+++ Main.gd
@@ -9,12 +9,19 @@ export var max_background_objects = 20
 
 var obstacles_scenes = [rock_scene, log_scene, bear_scene]
 var end_game = false
+var score = 0
+
+signal update_score(value)
 
 onready var player = $Player
 
 func _ready() -> void:
 	create_obstacle_object(true)
 	randomize()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		get_tree().reload_current_scene()
 
 func create_obstacle_object(force : bool = false):
 	if force or (randf() > 0.92 and get_tree().get_nodes_in_group("OBS").size() < max_obstacle_objects):
@@ -36,24 +43,44 @@ func check_killed_by_bear(value):
 		var obstacles = get_tree().get_nodes_in_group("OBS")
 		for obstacle in obstacles:
 			if obstacle.name == 'Bear':
-				end_game = true
-				print('YOU DIE - BEAR')
+				die("bear")
 	
+func die(reason):
+	end_game = true
+	var color
+	if reason == 'bear':
+		color  = Color8(169,39,39)
+	elif reason == 'obstracle':
+		color  = Color8(23,23,23)
+	elif reason == 'loud':
+		color = Color8(224,224,224)
+	else:
+		color = Color8(93,139,209)
+	$UserInterface/Retry.color = color
+	$UserInterface/Retry/Label2.text = "You were killed by " + reason
+	$UserInterface/Retry/Label3.text = "Score: " + String(score)
+	$UserInterface/Retry.show()
 
 func _on_AudioEnergy_audio_volume(value) -> void:
 	player.set_target_movement(value)
 	value *= 60
-	if value >= 15:
-		end_game = true
-		print("YOU DIE - TO LOUD")
+	check_killed_by_bear(value)
 	if not end_game:
-		check_killed_by_bear(value)
-		create_obstacle_object()
-		create_background_object()
-		get_tree().call_group("OBS", "move", value)
-		get_tree().call_group("BCK", "move", value)
-
+		if value >= 15:
+			die("loud")
+		else:
+			$KillTimer.stop()
+			create_obstacle_object()
+			create_background_object()
+			get_tree().call_group("OBS", "move", value)
+			get_tree().call_group("BCK", "move", value)
+			value = round(value)
+			emit_signal("update_score", value)
+			score += value
+			$KillTimer.start()
 
 func _on_Player_obstacle_on_way() -> void:
-	print("OBSTACLE - YOU DIE")
-	end_game = true
+	die("obstracle")
+
+func _on_KillTimer_timeout() -> void:
+	die("cold")
